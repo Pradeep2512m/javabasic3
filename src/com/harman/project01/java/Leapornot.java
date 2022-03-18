e0d6584730663b77f1dc1a00969b883c458797f9
pradeep2512m <pradeepm6383@gmail.com>
2022-03-18 09:19:46 +0530

    javabasic3

---
+++ src/com/harman/project01/java/Leapornot.java
@@ -0,0 +1,23 @@
+package com.harman.project01.java;
+import java.util.Scanner;
+
+public class Leapornot {
+
+    public static void main(String[] args) {
+
+        int year = 1998;
+
+
+        if (year % 400 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " is not a leap year.");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+    }
+}
+
